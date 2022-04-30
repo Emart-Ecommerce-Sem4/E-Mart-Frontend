e50b329840b79c1456a8c6f5e6ec9145064af297
Kaveesha Dinamidu <kaveesha.19@cse.mrt.ac.lk>
2022-04-30 19:22:20 +0530

    Homepage,signin,signup pages are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/homepage';
+import SignIn from './pages/signIn';
+import SignUp from './pages/signup';
 
 function App() {
   return (
     <div>
-      <h1>App</h1>
+      <Routes>
+        <Route path="/" element={<HomePage />} exact />
+        <Route path="/signin" element={<SignIn />} exact />
+        <Route path="/signup" element={<SignUp />} exact />
+      </Routes>
     </div>
   );
 }
