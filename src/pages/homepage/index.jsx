1e934e3e2956d13611716afcf048f55e71439622
yasirulakshan <yasiru.19@cse.mrt.ac.lk>
2022-05-10 13:29:39 +0530

    Main Image Added

--- src/pages/homepage/index.jsx
+++ src/pages/homepage/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import SignNavBar from "../../components/SignNavBar";
 
 export default function HomePage() {
+  
   return (
-    <div>
-      <h1>Home page</h1>
+    <div class="p-3 mb-2 bg-dark text-white">
+        <SignNavBar/>
+        <div><img src="./Home_Main.png" alt="" class="rounded mx-auto d-block" style={{marginTop:10}}/></div>
+        <br></br>
+        <h1>DEALS</h1> 
+
     </div>
   );
 }
