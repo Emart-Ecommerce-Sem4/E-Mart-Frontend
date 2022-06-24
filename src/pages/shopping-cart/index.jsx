7ff955aa3710f9ea766ac766bc5c375040f18f1a
Yasiru Lakshan <yasiru1999@gmail.com>
2022-06-24 15:41:46 +0530

    grid created

--- src/pages/shopping-cart/index.jsx
+++ src/pages/shopping-cart/index.jsx
@@ -1,9 +1,24 @@
+import { Grid, ListItem } from '@mui/material';
 import React from 'react';
+import SignNavBar from '../../components/SignNavBar';
 
 export default function ShoppingCart() {
   return (
-    <div>
-      <h1>Shopping Cart</h1>
+    <div class = "bg-dark text-white">
+      <div>
+        <SignNavBar />
+      </div>
+      <div style={{paddingLeft : '108px'}}>
+        <h1 style={{color : 'white'}}>SHOPPING CART</h1>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <ListItem>xs=6 md=8</ListItem>
+          </Grid>
+          <Grid item xs={6} >
+            <ListItem>xs=6 md=4</ListItem>
+          </Grid>
+        </Grid> 
+      </div>
     </div>
   );
 }
