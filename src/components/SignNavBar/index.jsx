ccdf477895b0aa74ba407e49bed315c88cc14e32
Chamod Madhusanka <chamodmadushanka115@gmail.com>
2022-06-25 14:07:58 +0530

    Design the sign up page

--- src/components/SignNavBar/index.jsx
+++ src/components/SignNavBar/index.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
+import { AiOutlineShoppingCart } from 'react-icons/ai';
 
 export default function SignNavBar() {
   return (
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
+        <div class="container-fluid" style={{paddingRight: 50}}>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" aria-current="page" href="./signin">Home</a>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link" href="#">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Contact Us</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#"><AiOutlineShoppingCart color="#dc3545" size={25} /></a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">
+                    <button type="button" class="btn btn-danger btn-sm">SIGNUP</button>
+                    </a>
                     </li>
                 
                 </ul>
--- src/pages/signup/index.jsx
+++ src/pages/signup/index.jsx
@@ -1,10 +1,157 @@
 import React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+// import { makeStyles } from "@material-ui/core/styles";
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import SignNavBar from "../../components/SignNavBar";
 
+const theme = createTheme();
+
+// const useStyles = makeStyles((theme) => ({
+//   cssLabel: {
+//     color: "#d3d3d3",
+//     "&.Mui-focused": {
+//       color: "#23A5EB"
+//     }
+//   },
+  
+// }));
+
 export default function SignUp() {
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  // const classes = useStyles();
+  
+
   return (
-    <div>
+    <Box sx={{paddingBottom: 25, backgroundColor: "#212529"}}>
+    <div class="bg-dark text-white">
       <SignNavBar />
+
+      <div class="container">
+        <div class="row align-items-start">
+          <div class="col">
+
+            <img src='./images/SignupImage.png' alt='signupImage' width={600} />
+          </div>
+
+          <div class="col">
+            <Container component="main" maxWidth="xs" >
+              <CssBaseline />
+              <Box
+                sx={{
+                  marginTop: 8,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  
+                }}
+              >
+                
+                <Typography component="h1" variant="h4">
+                  Register With Us!
+                </Typography>
+                <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                  <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                      <TextField
+                        autoComplete="given-name"
+                        name="Name"
+                        required
+                        fullWidth
+                        sx={{ input: { color: '#fff'}}}
+                        // InputLabelProps={{
+                        //   classes: {
+                        //     root: classes.cssLabel,
+                        //     focused: classes.cssFocused
+                        //   }
+                        // }}
+                        color="primary"
+                        id="Name"
+                        label="Name"
+                        autoFocus
+                        
+                        
+                      />
+                    </Grid>
+                    
+                    <Grid item xs={12}>
+                      <TextField
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="new-password"
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField
+                        required
+                        fullWidth
+                        name="confimPassword"
+                        label="Confirm Password"
+                        type="password"
+                        id="password"
+                        
+                      />
+                    </Grid>
+                    
+                  </Grid>
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                    color="warning"
+                  >
+                    Register
+                  </Button>
+                  <Grid container justifyContent="flex-end">
+                    <Grid item>
+                      <Link href="#" variant="body2">
+                        Already have an account? LOGIN
+                      </Link>
+                    </Grid>
+                  </Grid>
+                </Box>
+              </Box>
+              
+            </Container>
+
+            
+          </div>
+        </div>
+
+      </div>
     </div>
+    </Box>
   );
 }
