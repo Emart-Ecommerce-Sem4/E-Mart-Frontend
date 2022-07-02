f78ce9aa3e4b14e9d609a07d6325d15157d5cf6c
Kaveesha Dinamidu <kaveesha.19@cse.mrt.ac.lk>
2022-07-02 07:01:09 +0530

    Few changes done to views

--- src/pages/signup/index.jsx
+++ src/pages/signup/index.jsx
@@ -1,157 +1,141 @@
 import React from 'react';
-import Avatar from '@mui/material/Avatar';
+import { Formik } from 'formik';
+import { Stack } from '@mui/material';
+import * as Yup from 'yup';
 import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-// import { makeStyles } from "@material-ui/core/styles";
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import SignNavBar from "../../components/SignNavBar";
+import SignNavBar from '../../components/SignNavBar';
+import HeightBox from '../../components/HeightBox';
 
-const theme = createTheme();
-
-// const useStyles = makeStyles((theme) => ({
-//   cssLabel: {
-//     color: "#d3d3d3",
-//     "&.Mui-focused": {
-//       color: "#23A5EB"
-//     }
-//   },
-  
-// }));
+const validationSchema = Yup.object().shape({
+  name: Yup.string().min(4).max(20).required().label('Name'),
+  email: Yup.string().email().required().label('Email'),
+  password: Yup.string()
+    .min(8)
+    .max(15)
+    .required()
+    .label('Password')
+    .matches(/\d+/, 'Password should contain at least one number')
+    .matches(
+      /[a-z]+/,
+      'Password should contain at least one lowercase character'
+    )
+    .matches(
+      /[A-Z]+/,
+      'Passoword should contain at least one uppercase character'
+    )
+    .matches(
+      /[!@#$%^&*()-+]+/,
+      'Password should contain at least one special character'
+    ),
+});
 
 export default function SignUp() {
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
+  const initialValues = {
+    name: '',
+    email: '',
+    password: '',
   };
-
-  // const classes = useStyles();
-  
+  async function registerUser(values) {}
 
   return (
-    <Box sx={{paddingBottom: 25, backgroundColor: "#212529"}}>
-    <div class="bg-dark text-white">
-      <SignNavBar />
-
-      <div class="container">
-        <div class="row align-items-start">
-          <div class="col">
-
-            <img src='./images/SignupImage.png' alt='signupImage' width={600} />
+    <Box
+      sx={{ backgroundColor: '#212529', minHeight: '100vh', paddingLeft: 5 }}
+    >
+      <div>
+        <SignNavBar />
+        <HeightBox height={20} />
+        <Stack direction="row" spacing={10}>
+          <div>
+            <img src="./images/SignupImage.png" alt="signupImage" width={600} />
           </div>
+          <Formik>
+            {(formikProps) => {
+              const { values, handleChange, handleSubmit, errors, touched } =
+                formikProps;
+              return (
+                <div>
+                  <HeightBox height={20} />
+                  <Container component="main" maxWidth="xs">
+                    <Typography component="h1" variant="h4" color="#fff">
+                      Register With Us
+                    </Typography>
+                    <HeightBox height={30} />
+                    <Grid container spacing={2}>
+                      <Grid item xs={12}>
+                        <TextField
+                          autoComplete="given-name"
+                          name="Name"
+                          required
+                          fullWidth
+                          sx={{ input: { color: '#fff' } }}
+                          color="primary"
+                          id="Name"
+                          label="Name"
+                          autoFocus
+                        />
+                      </Grid>
 
-          <div class="col">
-            <Container component="main" maxWidth="xs" >
-              <CssBaseline />
-              <Box
-                sx={{
-                  marginTop: 8,
-                  display: 'flex',
-                  flexDirection: 'column',
-                  
-                }}
-              >
-                
-                <Typography component="h1" variant="h4">
-                  Register With Us!
-                </Typography>
-                <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-                  <Grid container spacing={2}>
-                    <Grid item xs={12}>
-                      <TextField
-                        autoComplete="given-name"
-                        name="Name"
-                        required
-                        fullWidth
-                        sx={{ input: { color: '#fff'}}}
-                        // InputLabelProps={{
-                        //   classes: {
-                        //     root: classes.cssLabel,
-                        //     focused: classes.cssFocused
-                        //   }
-                        // }}
-                        color="primary"
-                        id="Name"
-                        label="Name"
-                        autoFocus
-                        
-                        
-                      />
+                      <Grid item xs={12}>
+                        <TextField
+                          required
+                          fullWidth
+                          id="email"
+                          label="Email Address"
+                          name="email"
+                          autoComplete="email"
+                        />
+                      </Grid>
+                      <Grid item xs={12}>
+                        <TextField
+                          required
+                          fullWidth
+                          name="password"
+                          label="Password"
+                          type="password"
+                          id="password"
+                          autoComplete="new-password"
+                        />
+                      </Grid>
+                      <Grid item xs={12}>
+                        <TextField
+                          required
+                          fullWidth
+                          name="confimPassword"
+                          label="Confirm Password"
+                          type="password"
+                          id="password"
+                        />
+                      </Grid>
                     </Grid>
-                    
-                    <Grid item xs={12}>
-                      <TextField
-                        required
-                        fullWidth
-                        id="email"
-                        label="Email Address"
-                        name="email"
-                        autoComplete="email"
-                      />
+                    <Button
+                      type="submit"
+                      fullWidth
+                      variant="contained"
+                      sx={{ mt: 3, mb: 2 }}
+                      color="warning"
+                    >
+                      Register
+                    </Button>
+                    <Grid container justifyContent="flex-end">
+                      <Grid item>
+                        <Link href="#" variant="body2">
+                          Already have an account? LOGIN
+                        </Link>
+                      </Grid>
                     </Grid>
-                    <Grid item xs={12}>
-                      <TextField
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        autoComplete="new-password"
-                      />
-                    </Grid>
-                    <Grid item xs={12}>
-                      <TextField
-                        required
-                        fullWidth
-                        name="confimPassword"
-                        label="Confirm Password"
-                        type="password"
-                        id="password"
-                        
-                      />
-                    </Grid>
-                    
-                  </Grid>
-                  <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    sx={{ mt: 3, mb: 2 }}
-                    color="warning"
-                  >
-                    Register
-                  </Button>
-                  <Grid container justifyContent="flex-end">
-                    <Grid item>
-                      <Link href="#" variant="body2">
-                        Already have an account? LOGIN
-                      </Link>
-                    </Grid>
-                  </Grid>
-                </Box>
-              </Box>
-              
-            </Container>
-
-            
-          </div>
-        </div>
-
+                  </Container>
+                </div>
+              );
+            }}
+          </Formik>
+        </Stack>
       </div>
-    </div>
     </Box>
   );
 }
