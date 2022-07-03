f28d7520c69cfb533fb3731a886272b2e49eb3f7
Kaveesha Dinamidu <kaveesha.19@cse.mrt.ac.lk>
2022-07-04 00:30:55 +0530

    Admin dashboard added succesfully

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,12 @@
 export const HOST_URL = 'https://dbmsbackend.azurewebsites.net/api';
 export const TOKEN_KEY = 'ECOMMERCE_TOKEN';
 export const USER_DETAILS = 'USER_DETAILS';
+
+export const ADMIN_SECTIONS = {
+  DASHBOARD: 'dashboard',
+  ORDERS: 'orders',
+  CUSTOMERS: 'customers',
+  REPORTS: 'reports',
+  PRODUCTS: 'products',
+  CATEGORIES: 'categories',
+};
