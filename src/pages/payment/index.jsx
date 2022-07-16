2406d9f05001ff51914eee5fc45fd1d48a0d79b9
Chamod Madhusanka <chamodmadushanka115@gmail.com>
2022-07-16 15:45:14 +0530

    Refine the pages

--- src/pages/payment/index.jsx
+++ src/pages/payment/index.jsx
@@ -143,7 +143,7 @@ export default function Checkout() {
       <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
         <Paper
           variant="outlined"
-          sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}
+          sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 }, borderRadius: 10 }}
         >
           <Typography component="h1" variant="h4" align="center">
             Checkout
